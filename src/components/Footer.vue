1c92be4f71f72a51215c171e0a43f0d89b205523
orluk <alainwebdev@free.fr>
2022-10-11 16:37:13 +0200

    first commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+    <footer class="d-flex flex-row justify-content-center align-items-center">
+        <p>Copyright © 2022 Alain ORLUK / ID Formation</p>
+    </footer>
+</template>
+
+<style lang="scss" scoped>
+footer {
+    background-color: var(--gray-3);
+    color: var(--text-primary-color);
+}
+</style>
---
+++ src/data/product.ts
@@ -0,0 +1,74 @@
+export default [
+    {
+        id: 1,
+        image: 'src/assets/images/character_venti.JPG',
+        title: 'Venti',
+        reference:
+            'SHINF65244',
+        price: 69.90,
+    },
+    {
+        id: 2,
+        image: 'src/assets/images/character_mona.JPG',
+        title: 'Mona',
+        reference:
+            'SHINF99683',
+        price: 199.90,
+    },
+    {
+        id: 3,
+        image: 'src/assets/images/character_ningguang.JPG',
+        title: 'Ningguang',
+        reference:
+            'SHINF00408',
+        price: 434.90,
+    },
+    {
+        id: 4,
+        image: 'src/assets/images/character_keqing.JPG',
+        title: 'Keqing',
+        reference:
+            'SHINF20513',
+        price: 279.90,
+    },
+    {
+        id: 5,
+        image: 'src/assets/images/character_ganyu.JPG',
+        title: 'Ganyu',
+        reference:
+            'SHINF18389',
+        price: 234.90,
+    },
+    {
+        id: 6,
+        image: 'src/assets/images/character_barbara.JPG',
+        title: 'Barbara',
+        reference:
+            'SHINF54713',
+        price: 264.90,
+    },
+    {
+        id: 7,
+        image: 'src/assets/images/character_xiao.JPG',
+        title: 'Xiao',
+        reference:
+            'SHINF18361',
+        price: 339.90,
+    },
+    {
+        id: 8,
+        image: 'src/assets/images/character_lumine.JPG',
+        title: 'Lumine',
+        reference:
+            'SHINF65871',
+        price: 229.90,
+    },
+    {
+        id: 9,
+        image: 'src/assets/images/character_aether.JPG',
+        title: 'Aether',
+        reference:
+            'SHINF93223',
+        price: 229.90,
+    },
+  ];
